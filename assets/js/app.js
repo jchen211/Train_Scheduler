4f9a75612155918483141752fb29af3dd7c13bed
Jessica Chen <jchen@jchen.attlocal.net>
2019-01-25 13:14:41 -0600

    used bootstrap, added firebase config

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyAzEAChGpz6tp9c5I_c_an1NaajunE6cog",
+    authDomain: "work-1-c8100.firebaseapp.com",
+    databaseURL: "https://work-1-c8100.firebaseio.com",
+    projectId: "work-1-c8100",
+    storageBucket: "work-1-c8100.appspot.com",
+    messagingSenderId: "59521064535"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
